2c20e895e2e651d866ee6ebc6feee5c5f3a675f4
dreibh <dreibh@516812f1-d3fd-0310-9ea4-9aededebcb87>
2007-08-10 10:47:44 +0000

    Added add-dummy-interface and menu entries for local setup.
    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp/trunk/rserpooldemo@1504 516812f1-d3fd-0310-9ea4-9aededebcb87

--- src/canvas.h
+++ src/canvas.h
@@ -1,25 +1,36 @@
-/* $Id$ */
-/***************************************************************************
- *   The RSerPool Demo System                                              *
- *                                                                         *
+/* $Id$
+ * ##########################################################################
+ *
+ *              //===//   //=====   //===//   //       //   //===//
+ *             //    //  //        //    //  //       //   //    //
+ *            //===//   //=====   //===//   //       //   //===<<
+ *           //   \\         //  //        //       //   //    //
+ *          //     \\  =====//  //        //=====  //   //===//    Version II
+ *
+ *             ###################################################
+ *           ======  D E M O N S T R A T I O N   S Y S T E M  ======
+ *             ###################################################
+ *
+ * ############- An Efficient RSerPool Prototype Implementation #############
+ *
  *   Authors: Thomas Dreibholz, dreibh@exp-math.uni-essen.de               *
  *            Sebastian Rohde, rohde@exp-math.uni-essen.de                 *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: dreibh@iem.uni-due.de
+ */
 
 #ifndef CANVAS_H
 #define CANVAS_H
